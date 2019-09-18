4a03857f9529f1e4513bc70ebe37f65bc2dc7a4b
Zain Mansoor <zainmansoor77@gmail.com>
2019-09-19 00:39:55 +0500

    Add files via upload

---
+++ Variable type.py
@@ -0,0 +1,14 @@
+type('Hello, World!')
+
+type(17)
+
+type(3.2)
+
+type(3.2j)
+
+message = 'And now for something completely different'
+n = 17
+pi = 3.1415926535897931
+print(type(message))
+print(type(n))
+print(type(pi))
