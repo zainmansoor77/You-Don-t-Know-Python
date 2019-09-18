a3ab1133b0f81c6449e4a405360ba4ea75c6b177
Zain Mansoor <zainmansoor77@gmail.com>
2019-09-19 00:21:07 +0500

    Add files via upload

---
+++ comments.py
@@ -0,0 +1,21 @@
+# This is a comment.
+# This is another comment.
+# Python ignores these comments.
+# The code that Python executes is next, on line
+
+print("Hello, world!") # Greet the world
+
+'''
+This is a comment.
+This is another comment.
+Python ignores these comments.
+The code that Python executes is on line 7.
+'''
+
+minute = 45
+percentage = (minute * 100) / 60 # compute the percentage of the hour
+print(percentage)
+
+month = 90 # we can't use 0 in start of any number
+
+v = 5 # velocity in meters/second
