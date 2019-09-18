d8d6da6f7190763a4f29963554b839ca9476788f
Zain Mansoor <zainmansoor77@gmail.com>
2019-09-19 00:28:05 +0500

    Add files via upload

---
+++ Asking the user for input.py
@@ -0,0 +1,5 @@
+inp = input()
+print(inp)
+
+name = input('What is your name? ') # Before getting input from the user, it is a good idea to print a prompt telling the user what to input.
+print(name)
