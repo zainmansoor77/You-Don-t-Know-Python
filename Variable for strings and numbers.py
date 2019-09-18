4e7987453c87d7fe5c2090e52c9f08b3646bc4c6
Zain Mansoor <zainmansoor77@gmail.com>
2019-09-19 00:32:06 +0500

    Add files via upload

---
+++ Variable for strings and numbers.py
@@ -0,0 +1,33 @@
+name = "Mark" # python variable is created this way.
+print(name) # pyton displaying.
+
+name = "Mark"
+name = "Ace" # python could change the value inside the variable.
+print(name)
+
+floggle = "Mark" # instead of writing name = "Mark", floggle = "Mark" is also acceptable beacause python is blind to semantics
+print(floggle)
+
+thnx = "thanks for your input"
+print(thnx)
+
+weight=150 # unlike strings number is not enclosed is quotes
+print(weight+25)
+
+original_num = 23
+num_to_be_added = 7
+new_num = original_num + num_to_be_added  # python can do calculation totally based on variable
+print(new_num)
+
+original_num = 90
+original_num = original_num + 10 # a variable can be used to calculate its own new value
+print(original_num)
+
+your_age = "twenty"
+print(your_age)
+your_age = 20 # A variable in python could change its type
+print(your_age)
+
+a,b=2,3
+c = a*b
+print(c)
